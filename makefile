8502e045f51ff35d84987a5085872a66b05e1853
Michael Elgie <mbelgie@ualberta.ca>
2021-10-01 23:21:31 -0600

    small setup script

---
+++ makefile
@@ -0,0 +1,6 @@
+setup:
+	./setup.sh
+
+clean:
+	rm -rf data
+	rm -rf extras
--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
 # bash script for code setup
+SCRIPT_PATH=`realpath $0`
+SCRIPT_DIR=`dirname ${SCRIPT_PATH}`
+EXTRAS_DIR=${SCRIPT_DIR}/extras
+DATA_DIR=${SCRIPT_DIR}/data
+LOGS_DIR=${DATA_DIR}/logs
+
+
+echo "Creating directories..."
+mkdir -p ${EXTRAS_DIR}
+mkdir -p ${DATA_DIR}
+mkdir -p ${LOGS_DIR}
