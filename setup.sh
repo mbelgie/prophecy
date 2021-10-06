d8419e7ed96ac360087c4ef01009131b42088b0f
Michael Elgie <mbelgie@ualberta.ca>
2021-10-05 23:13:54 -0600

    update setup script

--- setup.sh
+++ setup.sh
@@ -5,6 +5,11 @@ EXTRAS_DIR=${SCRIPT_DIR}/extras
 DATA_DIR=${SCRIPT_DIR}/data
 LOGS_DIR=${DATA_DIR}/logs
 
+echo "Installing yfinance..."
+pip3 install yfinance
+
+echo "Installing matplotlib..."
+pip3 install matplotlib
 
 echo "Creating directories..."
 mkdir -p ${EXTRAS_DIR}
