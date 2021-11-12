9ccc393965339b261883761e4934c1bf0ce34a86
Michael Elgie <mbelgie@ualberta.ca>
2021-11-11 23:42:49 -0700

    Plotting data

--- code/srs/graphData.py
+++ code/srs/graphData.py
@@ -1,7 +1,46 @@
+from datetime import date
+import matplotlib.pyplot as plt
+import sys
 
+
+stockDataDir = "/home/michael/prophecy/prophecy/data/stocks/"
+
+
+def getData(ticker):
+    tickerFile = stockDataDir + str(ticker) + ".txt"
+    file = open(tickerFile, 'r')
+    lines = file.readlines()
+    file.close()
+    return lines
+
+
+def populateData(lines):
+    dateList = []
+    timeList = []
+    priceList = []
+    for line in lines:
+        data = line.split()
+        date = data[0]
+        time = data[1]
+        price = data[2]
+
+        dateList.append(date)
+        timeList.append(time)
+        priceList.append(price)
+    
+    return dateList, timeList, priceList
+        
+
+def plotData(dateList, timeList, priceList):
+    plt.plot(dateList, priceList)
+    plt.show()
+    
 
 def main():
-    pass
+    ticker = sys.argv[1]
+    lines = getData(ticker)
+    dateList, timeList, priceList = populateData(lines)
+    plotData(dateList, timeList, priceList)
 
 
 if __name__ == '__main__':
--- makefile
+++ makefile
@@ -8,5 +8,7 @@ clean:
 test:
 	python3 code/srs/getStockPrices.py -t lulu -s 2021-01-01 -e 2021-10-07 -i 60m
 	python3 code/srs/getStockPrices.py -t msft -s 2021-01-01 -e 2021-10-07 -i 60m
-	python3 code/srs/getStockPrices.py -t amzn -s 2021-01-01 -e 2021-10-07 -i 60m
+	python3 code/srs/getStockPrices.py -t amzn -s 2021-01-01 -e 2021-11-11 -i 60m
 	
+plot:
+	python3 code/srs/graphData.py amzn
