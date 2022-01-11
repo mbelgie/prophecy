a8818f72f0cac76273775f4b2b3d477d51cedbde
mbelgie <mbelgie@ualberta.ca>
2022-01-11 13:08:03 -0700

    added tools for cron jobs

---
+++ extras/updateData.py
@@ -0,0 +1,36 @@
+import sys
+import datetime
+import os
+
+debug = False
+stockDataDir = "/home/michael/prophecy/prophecy/data/stocks/"
+
+tickersFile = stockDataDir + "tickers.txt"
+
+
+# opens a file, reads all of the lines and then closes it
+def openFileRead(filename):
+    file = open(filename, 'r')
+    fileData = file.readlines()
+    file.close()
+    return fileData
+
+
+def buildCommandString(ticker):
+    commandString = "python3 ../code/srs/getStockPrices.py -t " + str(ticker) + " -s " + str(datetime.datetime.now().strftime("%Y-%m-%d")) + " -e " + \
+    str(datetime.date.today() + datetime.timedelta(days=1)) + " -i 5m"
+    
+    return commandString
+
+
+def main():
+    sys.path.insert(1, '../code/logger')
+    lines = openFileRead(tickersFile)
+    
+    for ticker in lines:
+        commandString = buildCommandString(ticker.strip())
+        os.system(str(commandString))
+
+
+if __name__ == '__main__':
+    main()
